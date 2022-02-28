3a1318a9099f1e0f55aa5900c07dc5b9c8c4e9d1
PietroCoelho <coelhopietro17@gmail.com>
2022-02-28 10:06:21 -0300

    feat: commit inicial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '07a6c05177ebd2d73c31eafe82dde50df2c98e82',
+    'name' => 'crud/php',
+  ),
+  'versions' => 
+  array (
+    'crud/php' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '07a6c05177ebd2d73c31eafe82dde50df2c98e82',
+    ),
+  ),
+);
